f5b51218cfa1537f8d13c17b0a209dc845c0e151
Jim Greco <jgreco@gmail.com>
2023-02-16 22:13:07 -0500

    wip

--- src/core/command.rs
+++ src/core/command.rs
@@ -1,6 +1,10 @@
+pub use values::CommandValueError;
+pub use values::CommandValueTypeError;
+pub use self::values::CommandValue;
 pub use self::parser::Parser;
 pub use self::parser::ParserError;
 pub use self::parser::ParserErrorType;
 
 mod lexer;
 pub mod parser;
+mod values;
